ce82bcf584b74fd0cf2f6754347e2e3aca9bb41d
andrey <andrey@glendarr.local>
2009-11-14 10:07:48 -0700

    print->println

--- p9test.go
+++ p9test.go
@@ -7,8 +7,8 @@ import (
 )
 
 func main() {
-	fmt.Print(p9.IOHdrSz);	// 24
-	fmt.Print(p9.Tremove);
-	fmt.Print(p9.OAPPEND);	// 0x4000
+	fmt.Println(p9.IOHdrSz);	// 24
+	fmt.Println(p9.Tremove);
+	fmt.Println(p9.OAPPEND);	// 0x4000
 	p9srv.ListenAndServe("tcp", "0.0.0.0:10000");
 }
