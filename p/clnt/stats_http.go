4e8ed96f0eb922c004272a3500a7277faee7731b
andrey <andrey@tieroko>
2010-10-29 13:01:46 -0600

    Various modifications for changes to strings.Split, logging (log.Stderr -> log.Println) and http.
    Fixes issue 5

--- p/clnt/stats_http.go
+++ p/clnt/stats_http.go
@@ -7,11 +7,26 @@ import (
 	"go9p.googlecode.com/hg/p"
 )
 
-func statsRegister() {
-	http.HandleFunc("/go9p/clnt", clntServeHTTP)
+
+
+func (clnt *Clnt) ServeHTTP(c http.ResponseWriter, r *http.Request) {
+	io.WriteString(c, fmt.Sprintf("<html><body><h1>Client %s</h1>", clnt.Id))
+	defer io.WriteString(c, "</body></html>")
+
+	// fcalls
+	if clnt.Debuglevel&DbgLogFcalls != 0 {
+		fs := clnt.Log.Filter(clnt, DbgLogFcalls)
+		io.WriteString(c, fmt.Sprintf("<h2>Last %d 9P messages</h2>", len(fs)))
+		for _, l := range fs {
+			fc := l.Data.(*p.Fcall)
+			if fc.Type != 0 {
+				io.WriteString(c, fmt.Sprintf("<br>%s", fc))
+			}
+		}
+	}
 }
 
-func clntServeHTTP(c *http.Conn, r *http.Request) {
+func clntServeHTTP(c http.ResponseWriter, r *http.Request) {
 	io.WriteString(c, fmt.Sprintf("<html><body>"))
 	defer io.WriteString(c, "</body></html>")
 
@@ -34,19 +49,9 @@ func (clnt *Clnt) statsUnregister() {
 	http.Handle("/go9p/clnt/"+clnt.Id, nil)
 }
 
-func (clnt *Clnt) ServeHTTP(c *http.Conn, r *http.Request) {
-	io.WriteString(c, fmt.Sprintf("<html><body><h1>Client %s</h1>", clnt.Id))
-	defer io.WriteString(c, "</body></html>")
 
-	// fcalls
-	if clnt.Debuglevel&DbgLogFcalls != 0 {
-		fs := clnt.Log.Filter(clnt, DbgLogFcalls)
-		io.WriteString(c, fmt.Sprintf("<h2>Last %d 9P messages</h2>", len(fs)))
-		for _, l := range fs {
-			fc := l.Data.(*p.Fcall)
-			if fc.Type != 0 {
-				io.WriteString(c, fmt.Sprintf("<br>%s", fc))
-			}
-		}
-	}
+func statsRegister() {
+	http.HandleFunc("/go9p/clnt", clntServeHTTP)
 }
+
+
