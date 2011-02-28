b3c7dc3c4a6b5db2546e1d34edd268b27c0a0b57
Latchesar Ionkov <lucho@ionkov.net>
2011-02-28 09:54:38 -0700

    Set the request error if it is _not_ already set.

--- p/clnt/clnt.go
+++ p/clnt/clnt.go
@@ -236,7 +236,7 @@ func (clnt *Clnt) recv() {
 					log.Println(fmt.Sprintf("TTT %v", r.Tc))
 					log.Println(fmt.Sprintf("RRR %v", r.Rc))
 				} else {
-					if r.Err != nil {
+					if r.Err == nil {
 						r.Err = &p.Error{r.Rc.Error, int(r.Rc.Errornum)}
 					}
 				}
