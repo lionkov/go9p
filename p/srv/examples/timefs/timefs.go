9e02d25b37198f6b8ab9722f4c954231156a8047
Andrey Mirtchovski <mirtchovski@gmail.com>
2012-10-10 13:36:50 -0600

    examples/time: fix crash when offset > 0
    this never really worked correctly, i guess.
    
    R=go9p, lucho
    CC=go9p
    http://codereview.appspot.com/6639059

--- p/srv/examples/timefs/timefs.go
+++ p/srv/examples/timefs/timefs.go
@@ -39,21 +39,15 @@ func (*InfTime) Read(fid *srv.FFid, buf []byte, offset uint64) (int, error) {
 }
 
 func (*Time) Read(fid *srv.FFid, buf []byte, offset uint64) (int, error) {
-	t := time.Now().String()
-	b := []byte(t)
-	n := len(b)
-	if offset >= uint64(n) {
+	b := []byte(time.Now().String())
+	have := len(b)
+	off := int(offset)
+
+	if off >= have {
 		return 0, nil
 	}
 
-	b = b[int(offset):n]
-	n -= int(offset)
-	if len(buf) < n {
-		n = len(buf)
-	}
-
-	copy(buf[offset:int(offset)+n], b[offset:])
-	return n, nil
+	return copy(buf, b[off:]), nil
 }
 
 func main() {
