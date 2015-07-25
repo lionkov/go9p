afd4027940199d820460918a092543e18e0f4ec1
Ronald G. Minnich <rminnich@gmail.com>
2015-07-24 23:04:15 -0700

    Get the ufs server to work with a Plan 9 terminal.
    This set of changes is tested with a Plan 9 terminal and works fine.
    
    I added the option of adding a user for people who want to set that. It's not required.
    I also make -root= actually work, but you have to use ufs.New() now. That's probably sensible
    since the ufs package has options of its own.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- p/srv/examples/ufs/ufs.go
+++ p/srv/examples/ufs/ufs.go
@@ -14,15 +14,22 @@ import (
 var (
 	debug = flag.Int("d", 0, "print debug messages")
 	addr = flag.String("addr", ":5640", "network address")
+	user = flag.String("user", "", "user name")
 )
 
 func main() {
 	flag.Parse()
-	ufs := new(ufs.Ufs)
+	ufs := ufs.New()
 	ufs.Dotu = true
 	ufs.Id = "ufs"
 	ufs.Debuglevel = *debug
 	ufs.Start(ufs)
+	if *user != "" {
+		u := ufs.Upool.Uname2User(*user)
+		if u == nil {
+			log.Printf("Warning: Adding %v failed", *user)
+		}
+	}
 
 	err := ufs.StartNetListener("tcp", *addr)
 	if err != nil {
